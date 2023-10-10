d3a45fc3066499511581bbb2d3cd71f9b605b130
Pratik Fuse <pratikfuse20@gmail.com>
2023-10-10 20:29:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const h1  = document.getElementsByTagName("h1")[0];
+let innerWidth = window.innerWidth;
+let innerHeight = window.innerHeight;
+h1.innerText = `Width: ${innerWidth} and Height: ${innerHeight}`;
+window.addEventListener('resize',()=>{
+	let innerWidth = window.innerWidth;
+let innerHeight = window.innerHeight;
+h1.innerText = `Width: ${innerWidth} and Height: ${innerHeight}`;
+});
